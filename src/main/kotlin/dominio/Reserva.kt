b54076c4a4c9cd663c2fd2394e77d2d2c4cef598
sromerop <sromerop@iesrafaelalberti.es>
2026-03-16 22:38:46 +0100

    Estructura y superclase reserva creados

---
+++ src/main/kotlin/dominio/Reserva.kt
@@ -0,0 +1,20 @@
+package dominio
+
+import java.time.LocalDateTime
+
+abstract class Reserva(val descripcion: String) {
+    val id: Int
+    val fechaCreacion: LocalDateTime = LocalDateTime.now()
+
+    companion object {
+        private var contador = 0
+    }
+
+    init {
+        contador += 1
+        id = contador
+    }
+
+    open val detalle: String
+        get() = "$id - $descripcion"
+}
